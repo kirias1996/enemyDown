f2983ebde86faafb41a629aef6b18ce02ff2a9d1
kirias1996 <arishou829@gmail.com>
2025-03-02 22:23:39 +0900

    新規作成

---
+++ src/main/java/plugin/enemyDown/Main.java
@@ -0,0 +1,15 @@
+package plugin.enemyDown;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+import plugin.enemyDown.command.EnemyDownCommand;
+
+public final class Main extends JavaPlugin  implements Listener{
+
+//  @Override
+  public void onEnable() {
+    Bukkit.getPluginManager().registerEvents(this, this);
+    getCommand("enemyDown").setExecutor(new EnemyDownCommand());
+  }
+}
