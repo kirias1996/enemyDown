ef514e3bab2c7f2815d087cca71730915849a9b4
kirias1996 <arishou829@gmail.com>
2025-03-15 22:57:43 +0900

    EnemySpawnCommandの新規作成

--- src/main/java/plugin/enemyDown/command/EnemySpawnCommand.java
+++ src/main/java/plugin/enemyDown/command/EnemySpawnCommand.java
@@ -1,4 +1,54 @@
 package plugin.enemyDown.command;
 
-public class EnemySpawnCommand {
+import java.util.List;
+import java.util.SplittableRandom;
+import org.bukkit.Location;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+
+public class EnemySpawnCommand extends BaseCommand implements Listener {
+
+
+  @Override
+  public boolean onExecutePlayerCommand(Player player) {
+    player.getWorld().spawnEntity(getEnemySpawnLocation(player), getEnemy());
+    return true;
+  }
+
+  @Override
+  public boolean onExecuteNPCCommand(CommandSender sender) {
+    return false;
+  }
+
+  /**
+   * 敵の出現エリアを取得します。 出現エリアはX軸とZ軸は自分の一からプラス,ランダムで-10~9の値が設定されます。 出現エリアはY軸はプレイヤーと同じ位置になります。
+   *
+   * @param player コマンドを実行したプレイヤー
+   * @param world  　コマンドを実行したプレイヤーが所属するワールド
+   * @return
+   */
+  private Location getEnemySpawnLocation(Player player) {
+    Location playerLocation = player.getLocation();
+    int randomX = new SplittableRandom().nextInt(10) - 5;
+    int randomZ = new SplittableRandom().nextInt(10) - 5;
+    double x = playerLocation.getX() + randomX;
+    double y = playerLocation.getY();
+    double z = playerLocation.getZ() + randomZ;
+
+    return new Location(player.getWorld(), x, y, z);
+  }
+
+  /**
+   * ランダムで敵を抽選してその結果の敵を取得します。
+   *
+   * @return　敵
+   */
+  private EntityType getEnemy() {
+    List<EntityType> enemyList = List.of(EntityType.ZOMBIE, EntityType.SPIDER, EntityType.SKELETON, EntityType.ZOMBIE_VILLAGER);
+    int random = new SplittableRandom().nextInt(enemyList.size());
+    return enemyList.get(random);
+  }
+
 }
